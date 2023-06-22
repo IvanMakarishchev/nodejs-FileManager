9a399c49d6ef248de71f7de23006c7aab748c80e
IvanMakarishchev <denis.flank@gmail.com>
2023-06-23 00:03:29 +0200

    feat: make dir module

--- src/modules/dir.js
+++ src/modules/dir.js
@@ -1,8 +1,9 @@
 import { userDir } from "../constants/environment.js";
-import { readdir } from "fs/promises";
+import { readdir, stat } from "fs/promises";
 import { messages } from "../constants/messages.js";
 import { checkPath } from "../utils/checkPath.js";
 import { argController } from "../utils/getArgs.js";
+import { lstatSync } from "fs";
 
 export class DirController {
   constructor() {
@@ -13,7 +14,9 @@ export class DirController {
     if (!(await checkPath(`${this.currentDir}\\${dirName}`))) {
       console.log(messages.unknownDir(dirName));
     } else {
-      this.currentDir = `${this.currentDir}\\${dirName}`;
+      (await stat(`${this.currentDir}\\${dirName}`)).isDirectory()
+        ? (this.currentDir = `${this.currentDir}\\${dirName}`)
+        : console.log(messages.unknownDir(dirName));
     }
   }
   upDir() {
@@ -22,9 +25,78 @@ export class DirController {
       : (this.currentDir = this.currentDir.split("\\").slice(0, -1).join("\\"));
   }
   async listDir() {
-    await readdir(this.currentDir, { withFileTypes: true }).then((data) =>
-      console.log(data.length)
-    );
+    await readdir(this.currentDir, { withFileTypes: true }).then((data) => {
+      let files = data.map((el) => {
+        const stat = lstatSync(`${this.currentDir}\\${el.name}`);
+        const pathType = {
+          directory: stat.isDirectory() ? true : false,
+          file: stat.isFile() ? true : false,
+          "block device": stat.isBlockDevice() ? true : false,
+          "char device": stat.isCharacterDevice() ? true : false,
+          fifo: stat.isFIFO() ? true : false,
+          socket: stat.isSocket() ? true : false,
+          "symbolic link": stat.isSymbolicLink() ? true : false,
+        };
+        return {
+          name: el.name,
+          type: Object.entries(pathType).find((el) => el[1])[0],
+        };
+      });
+      const maxFileLength = Math.max(...files.map((el) => el.name.length));
+      const maxFilesCount = Math.max(
+        `${files.length - 1}`.length,
+        "(index)".length
+      );
+      const maxTypeLength = Math.max(...files.map((el) => el.type.length));
+      let filesSorted = [
+        files.filter((el) => el.type === "directory"),
+        files.filter((el) => el.type === "block device"),
+        files.filter((el) => el.type === "char device"),
+        files.filter((el) => el.type === "fifo"),
+        files.filter((el) => el.type === "file"),
+        files.filter((el) => el.type === "socket"),
+        files.filter((el) => el.type === "symbolic link"),
+      ].flat();
+      filesSorted.forEach((el, i) => {
+        if (!i) {
+          console.log(
+            `╔${"═".repeat(maxFilesCount + 2)}╤${"═".repeat(
+              maxFileLength + 2
+            )}╤${"═".repeat(maxTypeLength + 2)}╗`
+          );
+          console.log(
+            `║ ${" "
+              .repeat(maxFilesCount)
+              .replace(" ".repeat(7), "(index)")} │ ${" "
+              .repeat(maxFileLength)
+              .replace(" ".repeat(4), "Name")} │ ${" "
+              .repeat(maxTypeLength)
+              .replace(" ".repeat(4), "Type")} ║`
+          );
+          console.log(
+            `╟${"─".repeat(maxFilesCount + 2)}┼${"─".repeat(
+              maxFileLength + 2
+            )}┼${"─".repeat(maxTypeLength + 2)}╢`
+          );
+        }
+        console.log(
+          `║ ${" "
+            .repeat(maxFilesCount)
+            .replace(" ".repeat(("" + i).length), i)} │ ${" "
+            .repeat(maxFileLength)
+            .replace(" ".repeat(el.name.length), el.name)} │ ${" "
+            .repeat(maxTypeLength)
+            .replace(" ".repeat(el.type.length), el.type)} ║`
+        );
+        if (i === filesSorted.length - 1) {
+          console.log(
+            `╚${"═".repeat(maxFilesCount + 2)}╧${"═".repeat(
+              maxFileLength + 2
+            )}╧${"═".repeat(maxTypeLength + 2)}╝`
+          );
+        }
+      });
+    });
   }
   getCurrentDir() {
     return this.currentDir;
